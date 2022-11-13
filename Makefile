34234c95be2f64fa564e70eee4f1f32ab6078b04
soraia <soraialobos@hotmail.com>
2022-11-13 22:19:46 +0000

    uppl

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/06 15:59:36 by somartin          #+#    #+#              #
-#    Updated: 2022/11/13 14:56:11 by somartin         ###   ########.fr        #
+#    Updated: 2022/11/13 19:15:18 by somartin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,6 +17,7 @@ SRCS			=	ft_memset.c ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memchr.c\
 					ft_strnstr.c ft_bzero.c ft_isascii.c \
 					ft_strlen.c  ft_strrchr.c \
 					ft_isdigit.c ft_tolower.c ft_toupper.c \
+					ft_substr.c ft_strjoin.c ft_strmapi.c ft_striteri.c ft_itoa.c ft_split.c ft_strtrim.c \
 					
 					OBJS			= $(SRCS:.c=.o)
 
---
+++ ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 19:10:59 by somartin          #+#    #+#             */
+/*   Updated: 2022/11/13 21:43:41 by somartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+	
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(*s) * (len + 1));
+	if (str == 0)
+		return (NULL);
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
