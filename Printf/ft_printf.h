d4023eea7bd57795f2dad11b401b56a635832fca
soraiathegirleffect <soraialobos@hotmail.com>
2022-12-08 18:28:12 +0000

    amazing job readyyyy

--- Printf/ft_printf.h
+++ Printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 19:37:41 by somartin          #+#    #+#             */
-/*   Updated: 2022/12/08 18:09:14 by somartin         ###   ########.fr       */
+/*   Updated: 2022/12/08 18:22:27 by somartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@
 # include <stdarg.h> 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stddef.h>
+# include <limits.h>
 
 int		ft_printf(const char *str, ...);
+int		ft_checkprintf(char str, va_list args);
 int		ft_putchar(char c);
 size_t	ft_strlen(const char *str);
 int		ft_putstr(char *s);
