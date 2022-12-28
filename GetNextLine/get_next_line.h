49da3615fb0e8130f6acb193e61dbdb156725b1a
soraia <soraialobos@hotmail.com>
2022-12-28 22:11:01 +0000

    jessica

--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -3,11 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: somartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 14:31:53 by somartin          #+#    #+#             */
-/*   Updated: 2022/12/10 14:31:57 by somartin         ###   ########.fr       */
+/*   Updated: 2022/12/28 21:18:48 by somartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# ifndef  BUFFER_SIZE
+#  define  BUFFER_SIZE 1
+# endif
 
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+int		ft_clean(char *str);
+
+#endif
