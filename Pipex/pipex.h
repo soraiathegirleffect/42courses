4793608cd55914bac264ccdddc22c67a0a194c87
soraia <soraialobos@hotmail.com>
2023-04-18 20:37:39 +0100

    fkmk

--- Pipex/pipex.h
+++ Pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 20:34:30 by somartin          #+#    #+#             */
-/*   Updated: 2023/04/14 20:43:52 by somartin         ###   ########.fr       */
+/*   Updated: 2023/04/18 18:20:25 by somartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <unistd.h> /*for close*/
 #include <stdlib.h> /*for perror, open, link*/
 #include <stdio.h> /*for exit, free, malloc*/
+#include <errno.h> /*for perror print a system error message*/
 # include <fcntl.h> /*for file control options and flags*/
 # include <sys/wait.h> /*for pid_t, waitpid*/
 
