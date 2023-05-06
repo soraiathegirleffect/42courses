1231a0feb05a0691cc6ed9fe0d7c781f9202db47
soraia <soraialobos@hotmail.com>
2023-05-06 14:08:35 +0100

    lets go

--- Push Swap/push_swap.c
+++ Push Swap/push_swap.c
@@ -6,21 +6,21 @@
 /*   By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 17:08:11 by somartin          #+#    #+#             */
-/*   Updated: 2023/04/30 15:46:56 by somartin         ###   ########.fr       */
+/*   Updated: 2023/05/06 14:03:58 by somartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
-#include "../libft/libft.h"
-#include "../printf/ft_printf.h"
+#include "push_swap.h"
+#include "Libft/libft.h"
+#include "Printf/ft_printf.h"
 #include <stdlib.h>
 
 void	push_swap(char **av)
 {
-	x_piles		pile; /* Declare pile as type x_piles */
+	x_pile		pile; /* Declare pile as type x_piles */
 	int			size;
 	int			i;
-	
+
 	i = 1;
 	size = ps_strlen(av);
 	pile.a = malloc(size * sizeof(int)); /* pile.specification */
--- Push Swap/push_swap.h
+++ Push Swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 17:02:19 by somartin          #+#    #+#             */
-/*   Updated: 2023/02/05 17:27:42 by somartin         ###   ########.fr       */
+/*   Updated: 2023/05/06 14:02:42 by somartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ typedef struct all_piles
 	int	size_a;
 	int	size_b;
 }	x_pile;
+
+#endif
 
 void	push_swap(char **av);
