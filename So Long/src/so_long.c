0be93dd6a06732c5afe770ac0316a06e2d4073ab
soraiathegirleffect <soraialobos@hotmail.com>
2023-05-06 20:51:08 +0100

    window working

--- So Long/src/so_long.c
+++ So Long/src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: somartin <somartin@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 20:27:19 by somartin          #+#    #+#             */
-/*   Updated: 2023/04/30 17:00:26 by somartin         ###   ########.fr       */
+/*   Updated: 2023/05/06 20:50:25 by somartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(void)
 {
 	t_win	tutorial;
 
-	tutorial = new_program(300, 300, "Soraia Game");
+	tutorial = new_program(300, 300, "somartin");
 	if (!tutorial.mlx_ptr || !tutorial.win_ptr)
 		return (1);
 	mlx_loop(tutorial.mlx_ptr);
